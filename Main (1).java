6485426bce402e33899afe86e8d8d85640b8bb55
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-04-20 11:07:22 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,53 @@
+ 
+ // No Parameter
+ class car{
+  car(){
+  System.out.println(" car is object created");
+}
+public static void main(String args[]){
+  car mycar = new car();
+}
+}
+
+
+
+
+class Main{
+  int x;
+  int y;
+  Main(int p, int q){
+    x = p;
+    y = q;
+    System.out.println("x:"+ x+",y:"+y);
+ 
+  }
+  public static void main(String args[]){
+    Main myobj = new Main(5,6);
+  
+  }
+}
+
+
+
+class piyush{
+  int x;
+  int y;
+  piyush(int p, int k){
+    x = p;
+    y = k;
+    System.out.println("x:"+x+",y"+y);
+  }
+  public static void main(String args[]){
+    piyush kumar = new piyush(10,20);
+  }
+}
+
+
+
+
+
+
+
+
+
+
